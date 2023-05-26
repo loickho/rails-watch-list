2fbd1d6589123dfe9150566766aa56ebe6ea9f32
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-05-26 12:21:45 +0200

    mvp

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,13 +6,22 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
     @bookmark.save
+    redirect_to list_path(@bookmark.list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
   end
 
   private
 
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # get 'lists', to: 'lists#index'
+
+  root 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
 end
